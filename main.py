b3bc92266780dab846f24f2d95a33f31ff19590e
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-05-13 17:37:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# 1-m
+class Student:
+    def __init__(self, fullname, grade):
+        self.fullname = fullname
+        self.__grade = grade
+
+    def get_grade(self):
+        return self.__grade
+
+    def set_grade(self, new_grade):
+        self.__grade = new_grade
+
+
+s1 = Student('Ali Valiyev', 4)
+
+print(s1.fullname)
+
+grade = s1.get_grade()
+print(grade)
+
+s1.set_grade(5)
+print(s1.get_grade())
